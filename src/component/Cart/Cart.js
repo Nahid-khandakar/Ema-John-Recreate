26a6d986d59ff9bbe6820601babeb9a6dbe277fd
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-03-23 04:49:56 +0600

    order summary ready

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './Cart.css'
+
 const Cart = ({ cart }) => {
     //console.log(props.cart)
+    let total = 0;
+    let shippingCharge = 0
+    // let tax = 0
+    // let grandTotal = 0
+    for (const product of cart) {
+        total = total + product.price;
+        shippingCharge = shippingCharge + product.shipping
+
+    }
+
+    const tax = parseFloat((total * 0.1).toFixed(2))
+    //console.log(typeof tax)
+
+    const grandTotal = total + shippingCharge + tax
+
+
     return (
-        <div>
+        <div className='cart'>
             <h3>Order Summary</h3>
             <p>Selected Item : {cart.length}</p>
+            <p>Total Price :${total} </p>
+            <p>Total Shipping Charge : ${shippingCharge} </p>
+            <p>Tax(10%) : ${tax}</p>
+
+            <h3>Grand Total : ${grandTotal}</h3>
         </div>
     );
 };
