30bb5394a7df00be1c4f8285abf085b046ca3f39
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-04-10 18:16:12 +0600

    sign up and login compnent add

---
+++ src/component/LogIn/LogIn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LogIn.css'
+
+const LogIn = () => {
+    return (
+        <div>
+            this is login
+        </div>
+    );
+};
+
+export default LogIn;
